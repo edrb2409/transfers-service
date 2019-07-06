242c3cff3b9ac784409365103cbd339c1a1481a1
Daniel Ron <edaniel.ron@outlook.com>
2019-07-06 00:19:13 -0500

    Add Jetty and Jersey bootstrap

---
+++ src/main/java/io/edrb/transferservice/TransferServiceApplication.java
@@ -0,0 +1,39 @@
+package io.edrb.transferservice;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TransferServiceApplication {
+
+    private static final Logger LOGGER = Logger.getLogger("TransferServiceApplication");
+
+    public static void main(String[] args) {
+        Server server = new Server(8080);
+
+        ServletContextHandler ctx = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+
+        ctx.setContextPath("/transfer-service");
+
+        server.setHandler(ctx);
+
+        ServletHolder servletHolder = ctx.addServlet(ServletContainer.class, "/api/*");
+        servletHolder.setInitOrder(1);
+        servletHolder.setInitParameter("jersey.config.server.provider.packages","io.edrb.transferservice.resource");
+
+        try {
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, null, e);
+        } finally {
+            server.destroy();
+        }
+
+    }
+
+}
